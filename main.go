a5bef770c095915fce55c3ae310bb17f33c60562
pew <83085672+hungrypotat@users.noreply.github.com>
2025-04-26 16:39:45 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,34 +1,24 @@
-/*
- * @Author: Vincent Yang
- * @Date: 2023-07-01 21:45:34
- * @LastEditors: Jason Lyu
- * @LastEditTime: 2025-04-08 13:45:00
- * @FilePath: /DeepLX/main.go
- * @Telegram: https://t.me/missuo
- * @GitHub: https://github.com/missuo
- *
- * Copyright © 2024 by Vincent, All Rights Reserved.
- */
-
 package main
 
 import (
 	"fmt"
-
-	"github.com/gin-gonic/gin"
-
-	"github.com/OwO-Network/DeepLX/service"
+	"net/http"
+	"os"
 )
 
 func main() {
-	cfg := service.InitConfig()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default if PORT not set
+	}
 
-	fmt.Printf("DeepL X has been successfully launched! Listening on %v:%v\n", cfg.IP, cfg.Port)
-	fmt.Println("Developed by sjlleo <i@leo.moe> and missuo <me@missuo.me>.")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello from DeepLX on Railway!")
+	})
 
-	// Setting the application to release mode
-	gin.SetMode(gin.ReleaseMode)
-
-	app := service.Router(cfg)
-	app.Run(fmt.Sprintf("%v:%v", cfg.IP, cfg.Port))
+	fmt.Println("Starting server on port " + port)
+	err := http.ListenAndServe(":" + port, nil)
+	if err != nil {
+		panic(err)
+	}
 }
